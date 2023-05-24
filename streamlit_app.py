6fa32a12ca97fdefece5a9d694815ea0e3633492
praveen07111996 <109718416+praveen07111996@users.noreply.github.com>
2023-05-24 12:24:05 +0530

    Update streamlit_app.py
    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title("Welcome to Pandiaraj Hotel")
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
