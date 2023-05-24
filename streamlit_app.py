fc861470ef64d74c3cd3ec4ae52976c105fedc5c
praveen07111996 <109718416+praveen07111996@users.noreply.github.com>
2023-05-24 12:22:20 +0530

    Update streamlit_app.py
    Added new

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("Hello Praveen !!! How are you ?/ this is called streaming")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
